1d56e63aa70dd92401bdc2b5d9287d4c26131227
resa <res.anal@gmail.com>
2022-03-24 22:29:23 +0530

    New Home Page

---
+++ new20k/assets/js/all.js
@@ -0,0 +1,36 @@
+/*!
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function(){
+	$(".nav-link").on('click', function(event) {
+
+    	if (this.hash !== "") {
+
+			event.preventDefault();
+
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 700, function(){
+				window.location.hash = hash;
+			});
+      	} 
+    });
+
+	/* let elem = document.getElementById("pills-onboard-tab");
+    if (elem) {
+      let tab = new bootstrap.Tab(elem);
+      tab.show();
+    } */
+});
